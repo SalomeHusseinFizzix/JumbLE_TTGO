5f06a268d72cb33cb20a3a4c6254da17ebfd1d52
Salomé <salome.hussein@riskfrontiers.com>
2022-09-21 21:23:20 +1000

    Implemented keyboard interrupt method

--- python_scripts/logIMU.py
+++ python_scripts/logIMU.py
@@ -1,18 +1,17 @@
 import serial
 import sys
 import time
-import numpy as np
 from datetime import datetime as dt
 
-port = 'COM5'
-if len(sys.argv) > 1:
-    port = sys.argv[1]
+port = 'COM6'
+# if len(sys.argv) > 1:
+#     port = sys.argv[1]
     
 lilygo_data = serial.Serial(port, 115200, timeout=1)
 time.sleep(2)
 if not lilygo_data.isOpen():
     lilygo_data.open()
-print('com5 is open', lilygo_data.isOpen())
+print('com6 is open', lilygo_data.isOpen())
 time.sleep(1)
 
 col_names = ['x_acc', 'y_acc', 'z_acc']
@@ -25,6 +24,7 @@ while True:
         b = lilygo_data.readline().strip()
         dataPacket=str(b,'utf-8')
         accel=dataPacket.split(',')
+        print(accel)
         #kludgy try-catch to ignore the print statement regarding bluetooth enabling
 
         if len(accel) == 3:
@@ -34,7 +34,7 @@ while True:
         break
 
 
-fn.close()
+fcsv.close()
 
 lilygo_data.close()
 
