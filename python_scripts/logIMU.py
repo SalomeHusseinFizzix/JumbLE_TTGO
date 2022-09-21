e6817aac34198b51af6f0a7f6234b821b36a88d0
Salomé <salome.hussein@riskfrontiers.com>
2022-09-21 21:59:51 +1000

    Experimenting with real-time data labelling from keyboard listening (PyGame/PyNput)

--- python_scripts/logIMU.py
+++ python_scripts/logIMU.py
@@ -2,7 +2,8 @@ import serial
 import sys
 import time
 from datetime import datetime as dt
-
+from pynput import keyboard
+import pygame as pg
 port = 'COM6'
 # if len(sys.argv) > 1:
 #     port = sys.argv[1]
@@ -14,10 +15,28 @@ if not lilygo_data.isOpen():
 print('com6 is open', lilygo_data.isOpen())
 time.sleep(1)
 
-col_names = ['x_acc', 'y_acc', 'z_acc']
+## functions from pynput documentation example, creates a Listener object that
+## needs care regarding its blocking
+# def on_press(key):
+#     try:
+#         print('alphanumeric key {0} pressed'.format(
+#             key.char))
+#     except AttributeError:
+#         print('special key {0} pressed'.format(
+#             key))
+#
+# def on_release(key):
+#     print('{0} released'.format(
+#         key))
+#     if key == keyboard.Key.esc:
+#         # Stop listener
+#         return False
+
+col_names = ['x_acc', 'y_acc', 'z_acc', 'label']
 fn = f'data/IMUdata_{dt.now().strftime("%d%m%Y")}_{dt.now().strftime("%H%M%S")}.csv'
 fcsv = open(fn, 'w+')
 fcsv.write(','.join(col_names) + '\n')
+
 
 while True:
     try:
@@ -25,12 +44,16 @@ while True:
         dataPacket=str(b,'utf-8')
         accel=dataPacket.split(',')
         print(accel)
-        #kludgy try-catch to ignore the print statement regarding bluetooth enabling
 
+        if pg.key.get_focused == True:
+            name = pg.key.name()
+            print(name)
+        else:
+            name = 'nan'
         if len(accel) == 3:
-            fcsv.write(dataPacket + '\n')
-    except:
-        print("KeyboardInterrupt")
+            fcsv.write(f'{dataPacket}, {name}' + '\n')
+    except Exception as e:
+        print(f"KeyboardInterrupt or {e}")
         break
 
 
