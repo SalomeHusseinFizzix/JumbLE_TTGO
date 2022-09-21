18dcf4c023f601c79a778c38f23411001ea59444
vlimit <stuart.warren@uts.edu.au>
2022-09-22 06:02:30 +1000

    Still can't get keyboard input to work. Added timestamp and marker types for log format.

--- python_scripts/logIMU.py
+++ python_scripts/logIMU.py
@@ -1,18 +1,21 @@
+import logging
 import serial
 import sys
 import time
 from datetime import datetime as dt
-from pynput import keyboard
 import pygame as pg
+
 port = 'COM6'
-# if len(sys.argv) > 1:
-#     port = sys.argv[1]
+
+if len(sys.argv) > 1:
+    port = sys.argv[1]
     
 lilygo_data = serial.Serial(port, 115200, timeout=1)
 time.sleep(2)
 if not lilygo_data.isOpen():
     lilygo_data.open()
-print('com6 is open', lilygo_data.isOpen())
+print('%s is open' % (port), lilygo_data.isOpen())
+
 time.sleep(1)
 
 ## functions from pynput documentation example, creates a Listener object that
@@ -35,26 +38,47 @@ time.sleep(1)
 col_names = ['x_acc', 'y_acc', 'z_acc', 'label']
 fn = f'data/IMUdata_{dt.now().strftime("%d%m%Y")}_{dt.now().strftime("%H%M%S")}.csv'
 fcsv = open(fn, 'w+')
+
+logging.info('Loging to %s.' % (fn))
+
 fcsv.write(','.join(col_names) + '\n')
 
+class Marker:
+    AccelData = 1
+    Keyboard = 2
+
+pg.init()
 
 while True:
-    try:
         b = lilygo_data.readline().strip()
+        t = time.time()
         dataPacket=str(b,'utf-8')
         accel=dataPacket.split(',')
-        print(accel)
 
-        if pg.key.get_focused == True:
-            name = pg.key.name()
-            print(name)
-        else:
-            name = 'nan'
+#        for ev in pg.event.get():
+#            if ev.type == pg.KEYDOWN:
+#                print('key;)')
+
+#        pg.event.pump()
+#        for event in pg.event.get():
+#            print('e')
+
+        keys = pg.key.get_pressed()
+        
+        if True in keys:
+#            name = pg.key.name()
+            print('k')
+            fields = [t, Marker.Keyboard, 'a']
+            fcsv.write(','.join(fields) + '\n')
+
         if len(accel) == 3:
-            fcsv.write(f'{dataPacket}, {name}' + '\n')
-    except Exception as e:
-        print(f"KeyboardInterrupt or {e}")
-        break
+#            print('.')
+            fields = [t, Marker.AccelData] + accel
+            fcsv.write(','.join(str(f) for f in fields) + '\n')
+
+#    except Exception as e:
+#        print(f"KeyboardInterrupt or {e}")
+#        break
 
 
 fcsv.close()
