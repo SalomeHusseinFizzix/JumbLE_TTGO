35b3531ac3087bc40afc601b40b4fbc6aff4405b
vlimit <stuart.warren@uts.edu.au>
2022-09-22 15:42:03 +1000

    Log key strokes, show some counts and use the logging module.

--- python_scripts/logIMU.py
+++ python_scripts/logIMU.py
@@ -1,45 +1,35 @@
 import logging
+import msvcrt
+import os.path
 import serial
 import sys
 import time
 from datetime import datetime as dt
-import pygame as pg
+
+getch = msvcrt.getch
+
+logging.basicConfig(level=logging.DEBUG)
 
 port = 'COM6'
 
 if len(sys.argv) > 1:
     port = sys.argv[1]
-    
+
 lilygo_data = serial.Serial(port, 115200, timeout=1)
-time.sleep(2)
+#time.sleep(2)
+
 if not lilygo_data.isOpen():
     lilygo_data.open()
-print('%s is open' % (port), lilygo_data.isOpen())
 
-time.sleep(1)
+logging.info('Opened %s.' % (port))
 
-## functions from pynput documentation example, creates a Listener object that
-## needs care regarding its blocking
-# def on_press(key):
-#     try:
-#         print('alphanumeric key {0} pressed'.format(
-#             key.char))
-#     except AttributeError:
-#         print('special key {0} pressed'.format(
-#             key))
-#
-# def on_release(key):
-#     print('{0} released'.format(
-#         key))
-#     if key == keyboard.Key.esc:
-#         # Stop listener
-#         return False
+#time.sleep(1)
 
 col_names = ['x_acc', 'y_acc', 'z_acc', 'label']
-fn = f'data/IMUdata_{dt.now().strftime("%d%m%Y")}_{dt.now().strftime("%H%M%S")}.csv'
+fn = os.path.join('data', f'IMUdata_{dt.now().strftime("%d%m%Y")}_{dt.now().strftime("%H%M%S")}.csv')
 fcsv = open(fn, 'w+')
 
-logging.info('Loging to %s.' % (fn))
+logging.info('Logging data to %s.' % (fn))
 
 fcsv.write(','.join(col_names) + '\n')
 
@@ -47,39 +37,31 @@ class Marker:
     AccelData = 1
     Keyboard = 2
 
-pg.init()
+t0 = time.time()
+nsamples = 0
+print_ts = 0
 
 while True:
         b = lilygo_data.readline().strip()
         t = time.time()
-        dataPacket=str(b,'utf-8')
+        dataPacket=str(b, 'utf-8')
         accel=dataPacket.split(',')
 
-#        for ev in pg.event.get():
-#            if ev.type == pg.KEYDOWN:
-#                print('key;)')
-
-#        pg.event.pump()
-#        for event in pg.event.get():
-#            print('e')
-
-        keys = pg.key.get_pressed()
-        
-        if True in keys:
-#            name = pg.key.name()
-            print('k')
-            fields = [t, Marker.Keyboard, 'a']
-            fcsv.write(','.join(fields) + '\n')
-
-        if len(accel) == 3:
-#            print('.')
-            fields = [t, Marker.AccelData] + accel
+        if msvcrt.kbhit():
+            c = str(getch(), 'utf-8')
+            fields = [t, Marker.Keyboard, c]
+            print()
+            logging.info('Key "%s" logged @%fs.' % (c, t-t0))
             fcsv.write(','.join(str(f) for f in fields) + '\n')
 
-#    except Exception as e:
-#        print(f"KeyboardInterrupt or {e}")
-#        break
+        if len(accel) == 3:
+            fields = [t, Marker.AccelData] + accel
+            fcsv.write(','.join(str(f) for f in fields) + '\n')
+            nsamples += 1
 
+            if t >= print_ts + 0.1:
+                print('\r %3.1f: %d' % (t-t0, nsamples), end='')
+                print_ts = t
 
 fcsv.close()
 
