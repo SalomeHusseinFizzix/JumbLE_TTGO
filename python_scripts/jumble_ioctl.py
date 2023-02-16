b52eb2ac12a7510c08b302d8a98e40784e6401d7
Salomé <salome.hussein@riskfrontiers.com>
2023-02-16 16:58:20 +1100

    Aded datalogging to haptic control

--- python_scripts/jumble_ioctl.py
+++ python_scripts/jumble_ioctl.py
@@ -24,9 +24,25 @@ if not lilygo_data.isOpen():
     lilygo_data.open()
 
 logging.info('Opened %s.' % (port))
+#setting up logging file for imu data
+col_names = ['time', 'marker', 'data_1', 'data_2', 'data_3']
+fn = os.path.join('data', f'IMUdata_{dt.now().strftime("%d%m%Y")}_{dt.now().strftime("%H%M%S")}.csv')
+fcsv = open(fn, 'w+')
 
+logging.info('Logging data to %s.' % (fn))
+
+fcsv.write(','.join(col_names) + '\n')
+class Marker:
+    LogData   = 0
+    AccelData = 1
+    Keyboard  = 2
+
+t0 = time.time()
+nsamples = 0
+print_ts = 0
 deadline = time.time()
-INTERVAL = 10
+#frequency of vibe
+INTERVAL = 3
 
 while True:
     if time.time() > deadline:
@@ -36,5 +52,38 @@ while True:
         print('Done')
 
     b = lilygo_data.readline().strip()
-    print(b)
+    t = time.time()
+    dataPacket = str(b, 'utf-8')
+    accel = dataPacket.split(',')
+    if msvcrt.kbhit():
+        c = str(getch(), 'utf-8')
+        fields = [t, Marker.Keyboard, c]
+        print()
+        logging.info('Key "%s" logged @%fs.' % (c, t - t0))
+        fcsv.write(','.join(str(f) for f in fields) + '\n')
 
+    if len(accel) == 3:
+        fail = False
+        for i in range(len(accel)):
+            try:
+                accel[i] = int(accel[i])
+            except:
+                fail = True
+        if not fail:
+            fields = [t, Marker.AccelData] + accel
+            fcsv.write(','.join(str(f) for f in fields) + '\n')
+            nsamples += 1
+
+            if t >= print_ts + 0.1:
+                print('\r %3.1f: %d' % (t - t0, nsamples), end='')
+                print_ts = t
+            continue
+
+    # Treat anything else as log data
+    fields = [t, Marker.LogData, '"%s"' % (dataPacket)]
+    fcsv.write(','.join(str(f) for f in fields) + '\n')
+    print()
+    logging.info(dataPacket)
+
+fcsv.close()
+lilygo_data.close()
